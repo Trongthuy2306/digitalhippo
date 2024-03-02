0d89cd107a01aec90354b68ba502c010319b629d
Trongthuy2306 <thuynguyen.23062002@gmail.com>
2024-03-02 16:45:26 +0700

    update

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
 			},
 			{
 				protocol: "https",
-				hostname: "digitalhippo-production.up.railway.app",
+				hostname: "digitalhippo-production-72ea.up.railway.app",
 			},
 		],
 	},
